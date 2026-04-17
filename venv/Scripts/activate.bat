e275d0ec93208da5b302d3cf27269d02c3edc70b
Clay José Ribeiro soares <ti.claysoares@mfdigital.net>
2026-04-17 11:04:18 -0300

    refactor: reorganizar estrutura de notebooks e pastas de dados

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\clayj\Desktop\observatorio-saude-urbana\venv"
+set "VIRTUAL_ENV=C:\Users\ti.claysoares\Documents\observatorio-saude-urbana\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
